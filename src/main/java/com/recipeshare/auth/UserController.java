60c3cd90b5083e5e4f44f69f91743fb545b61d30
Ethan McNeil <mcneil.ethan@gmail.com>
2020-10-06 16:34:45 -0400

    updates to user

--- src/main/java/com/recipeshare/auth/UserController.java
+++ src/main/java/com/recipeshare/auth/UserController.java
@@ -1,11 +1,14 @@
 package com.recipeshare.auth;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,7 +32,7 @@ private SqlUserDetailsService userService;
  public ResponseEntity<User> getInfo(@RequestHeader String username) {
 	 User foundUser = userService.loadFullUserByUsername(username);
 	 return ResponseEntity.ok(foundUser);
- }
+ };
 
 
 @DeleteMapping("/{id}")
@@ -43,7 +46,17 @@ public ResponseEntity<User> deleteUser(@PathVariable(value="id") Integer id) {
 	   userService.Delete(foundUser);
        return ResponseEntity.ok().build();
    }
-
-}
-
+};
+@PutMapping(path="/my-recipe")
+public ResponseEntity<User> updateMyRecipe(@RequestHeader int userId,@RequestHeader int recipeId) {
+	 User foundUser = userService.loadUserById(userId);
+	 ArrayList<Integer> savedRecipes = foundUser.getSavedRecipes();
+	 if(savedRecipes == null ) {
+		 savedRecipes = new ArrayList<Integer> ();
+	 }
+	 	savedRecipes.add(recipeId);
+	 	foundUser.setSavedRecipes(savedRecipes);
+	 	userService.Update(foundUser);
+	 return ResponseEntity.ok(foundUser);
+    };
 }
